29ffc0dacd1e8332cf9cd6af55418de9be67ecc0
Ilya Bekirov <ilja.bekirov@gmail.com>
2019-10-19 02:14:00 +0300

    Parse orders input

--- app.rb
+++ app.rb
@@ -6,6 +6,9 @@ require 'sinatra/activerecord'
 set :database, 'sqlite3:pizzashop.db'
 
 class Product < ActiveRecord::Base
+  def find_by_id(id)
+    Product.find(id: id)
+  end
 end
 
 get '/' do
@@ -18,5 +21,20 @@ get '/about' do
 end
 
 post '/cart' do
-  erb 'Hello World'
+  orders_input = params[:orders]
+  @orders = parse_orders_line(orders_input)
+  erb :cart
+end
+
+def parse_orders_line(orders_input)
+  arr = []
+  orders_input = orders_input.split(',')
+  orders_input.map do |order|
+    order = order.split('=')
+    product = order[0].split('_')
+    id = product[1]
+    cnt = order[1]
+    arr << [id, cnt]
+  end
+  arr
 end
